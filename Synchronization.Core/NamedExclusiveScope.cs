9730079c3e105778402bbdcf8edb6b6faa7812c3
nirzaf <mfmfazrin1986@gmail.com>
2021-12-17 13:33:37 +0530

    Add project files.

---
+++ Synchronization.Core/NamedExclusiveScope.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Synchronization.Core
+{
+    /*
+     * Implement very simple wrapper around Mutex or Semaphore (remember both implement WaitHandle) to
+     * provide a exclusive region created by `using` clause.
+     *
+     * Created region may be system-wide or not, depending on the constructor parameter.
+     *
+     * Any try to get a second systemwide scope should throw an `System.InvalidOperationException` with `Unable to get a global lock {name}.`
+     */
+    public class NamedExclusiveScope : IDisposable
+    {
+        public NamedExclusiveScope(string name, bool isSystemWide)
+        {
+        }
+    }
+}
